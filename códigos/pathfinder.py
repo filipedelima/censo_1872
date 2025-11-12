2151ed17ae0c00b81208b423ce17fb19e3792f5d
filipedelima <filipedelima@gmail.com>
2025-11-12 11:35:38 -0300

    Atualizando pathfinder.py e fazendo devidas alterações nos códigos

--- códigos/pathfinder.py
+++ códigos/pathfinder.py
@@ -1,8 +1,8 @@
 import os
 
-def find_file(filename):
+def find_file(filename, folder):
     file_path = os.getcwd()
-    file_path = file_path.replace("códigos", "planilhas")
+    file_path = file_path.replace("códigos", folder)
     for files in os.listdir(file_path):
             if filename in files:
                 file_path = os.path.join(file_path, filename)
